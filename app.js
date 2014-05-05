c873c1beaf87e37a8592fd2adc23248222caec53
erikaji <erikaji@stanford.edu>
2014-05-05 16:03:42 -0700

    mysql setup

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
+var mysql      = require('mysql');
 
 var results = require('./routes/results');
 var article = require ('./routes/article');
-var external = require ('./routes/external');
 var choosemood = require('./routes/choosemood');
 
 var app = express();
@@ -37,10 +37,18 @@ var hbs = handlebars.create({
     }
 });
 
+//mysql reference - https://github.com/felixge/node-mysql
+var connection = mysql.createConnection({
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "ba3bd950dbfbed", 
+    password: "573b449f"
+});
+
+connection.query('USE heroku_f6c3e56bf244b8e');
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-//app.engine('handlebars', handlebars());
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.favicon());
@@ -48,7 +56,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('readbymood secret key'));
+app.use(express.cookieParser('rosebt secret key'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -60,15 +68,14 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', choosemood.view);
-app.get('/experiment', choosemood.viewExperiment);
+//app.get('/', function(req, res){
+//  connection.query('SELECT * FROM users', function(err, rows){
+    //res.render('users', {users : rows});
+  //});
+//});
 app.get('/results', results.view);
-app.get('/resultsExperiment', results.viewExperiment);
 app.get('/article/:id', article.view);
-app.get('/articleExperiment/:id', article.viewExperiment);
-app.get('/external/:id', external.view);
-app.get('/externalExperiment/:id', external.viewExperiment);
 app.post('/choosemood', choosemood.post);
-app.post('/choosemoodExperiment', choosemood.postExperiment);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
