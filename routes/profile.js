88d53700cd0e53a579df222b73c7b40980d2eee7
erikaji <erikaji@stanford.edu>
2014-05-08 00:49:17 -0700

    photo URL edit

--- routes/profile.js
+++ routes/profile.js
@@ -12,11 +12,14 @@ exports.post = function(req, res){
 	connection.query('UPDATE rbt SET rose="'+req.body.rose+'" WHERE id_rbt=3', function(err, rows) {
 		connection.query('UPDATE rbt SET bud="'+req.body.bud+'" WHERE id_rbt=3', function(err, rows) {
 			connection.query('UPDATE rbt SET thorn="'+req.body.thorn+'" WHERE id_rbt=3', function(err, rows) {
+
+			connection.query('UPDATE rbt SET photo_rbt="'+req.body.photo+'" WHERE id_rbt=3', function(err, rows) {
 	 			connection.query('SELECT * FROM (rbt, user) WHERE (rbt.id_user = user.id_user AND rbt.id_user = 2)', function(err, rows_rbt){
 				    res.render('profile', {
 				      rbt: rows_rbt
 				    });
 				});
+	 		});
 			});
 		});
 	  });
