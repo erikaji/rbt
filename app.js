ccd6ced9b19f64f00abedffad0523d98209f5dba
yijuhou <this.yiju@gmail.com>
2014-05-07 22:32:40 -0700

    add two js files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
 var mysql      = require('mysql');
+var crypto = require('crypto');
 
 // var feed = require('./routes/feed');
 // var profile = require('./routes/profile');
@@ -34,6 +35,49 @@ var hbs = handlebars.create({
             return ret;
             }
     }
+});
+
+
+/*
+ * Respond to GET requests to /sign_s3.
+ * Upon request, return JSON containing the temporarily-signed S3 request and the
+ * anticipated URL of the image.
+ */
+app.get('/sign_s3', function(req, res){
+    var object_name = req.query.s3_object_name;
+    var mime_type = req.query.s3_object_type;
+
+    var now = new Date();
+    var expires = Math.ceil((now.getTime() + 10000)/1000); // 10 seconds from now
+    var amz_headers = "x-amz-acl:public-read";  
+
+    var put_request = "PUT\n\n"+mime_type+"\n"+expires+"\n"+amz_headers+"\n/"+S3_BUCKET+"/"+object_name;
+
+    var signature = crypto.createHmac('sha1', AWS_SECRET_KEY).update(put_request).digest('base64');
+    signature = encodeURIComponent(signature.trim());
+    signature = signature.replace('%2B','+');
+
+    var url = 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+object_name;
+
+    var credentials = {
+        signed_request: url+"?AWSAccessKeyId="+AWS_ACCESS_KEY+"&Expires="+expires+"&Signature="+signature,
+        url: url
+    };
+    res.write(JSON.stringify(credentials));
+    res.end();
+});
+
+/*
+ * Respond to POST requests to /submit_form.
+ * This function needs to be completed to handle the information in 
+ * a way that suits your application.
+ */
+app.post('/submit_form', function(req, res){
+    username = req.body.username;
+    full_name = req.body.full_name;
+    avatar_url = req.body.avatar_url;
+    update_account(username, full_name, avatar_url); // TODO: create this function
+    // TODO: Return something useful or redirect
 });
 
 //mysql reference - https://github.com/felixge/node-mysql
