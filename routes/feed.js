f339ce8491e26d866bb7090070458d5321742c74
erikaji <erikaji@stanford.edu>
2014-05-21 03:32:51 -0700

    feed navbars and temp not-current-user hack done

--- routes/feed.js
+++ routes/feed.js
@@ -1,7 +1,7 @@
 exports.view = function(req, res){
 	var pool = req.app.get('pool');
 	pool.getConnection(function(err, connection) {
-	    connection.query('SELECT * FROM (rbt, user) WHERE (rbt.id_user = user.id_user) ORDER BY "created_at" DESC', function(err, rows_rbt) {
+	    connection.query('SELECT * FROM (rbt, user) WHERE (rbt.id_user = user.id_user AND rbt.id_user != 2) ORDER BY "created_at" DESC', function(err, rows_rbt) {
 	    	res.render('feed', {
 				rbt: rows_rbt
 			});
