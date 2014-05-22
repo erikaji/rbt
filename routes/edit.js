70eb1c8d0c1303f3c0bd7d34104a690a82f71285
erikaji <erikaji@stanford.edu>
2014-05-21 20:18:03 -0700

    frontend UI for profile page done

--- routes/edit.js
+++ routes/edit.js
@@ -1,7 +1,7 @@
 exports.view = function(req, res){
 	var pool = req.app.get('pool');
 	pool.getConnection(function(err, connection) {
-	    connection.query('SELECT * FROM (rbt, user) WHERE (rbt.id_user = user.id_user AND rbt.id_user = 2)', function(err, rows_rbt) {
+		connection.query('SELECT id_user, firstname, lastname, photo_user, facebook_id FROM user WHERE (id_user = 2)', function(err, rows_rbt) {
 	    	res.render('edit', {
 				rbt: rows_rbt
 			});
--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,7 @@
 exports.view = function(req, res){
 	var pool = req.app.get('pool');
 	pool.getConnection(function(err, connection) {
-	    connection.query('SELECT * FROM (rbt, user) WHERE (rbt.id_user = user.id_user AND rbt.id_user = 2)', function(err, rows_rbt) {
+		connection.query('SELECT id_rbt, rbt.id_user, DATE_FORMAT(created_at, "%Y-%m-%dT%TZ") as created_at, photo_rbt, rose, bud, thorn, user.id_user, firstname, lastname, photo_user, facebook_id FROM (rbt, user) WHERE (rbt.id_user = user.id_user AND rbt.id_user = 2)', function(err, rows_rbt) {
 	    	res.render('profile', {
 				rbt: rows_rbt
 			});
@@ -17,7 +17,7 @@ exports.post = function(req, res){
 			connection.query('UPDATE rbt SET bud="'+req.body.bud+'" WHERE id_rbt=3', function(err, rows) {
 				connection.query('UPDATE rbt SET thorn="'+req.body.thorn+'" WHERE id_rbt=3', function(err, rows) {
 					connection.query('UPDATE rbt SET photo_rbt="'+req.body.photo+'" WHERE id_rbt=3', function(err, rows) {
-	 					connection.query('SELECT * FROM (rbt, user) WHERE (rbt.id_user = user.id_user AND rbt.id_user = 2)', function(err, rows_rbt){
+						connection.query('SELECT id_rbt, rbt.id_user, DATE_FORMAT(created_at, "%Y-%m-%dT%TZ") as created_at, photo_rbt, rose, bud, thorn, user.id_user, firstname, lastname, photo_user, facebook_id FROM (rbt, user) WHERE (rbt.id_user = user.id_user AND rbt.id_user = 2)', function(err, rows_rbt){
 						    res.render('profile', {
 						    	rbt: rows_rbt
 				    		});
