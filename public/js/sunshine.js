f2a8469c956e82ef487055c0845f6ed87da52f7e
erikaji <erikaji@stanford.edu>
2014-06-03 06:51:09 -0700

    sunshine frontend in progress

--- public/js/sunshine.js
+++ public/js/sunshine.js
@@ -2,6 +2,7 @@ function give_sun(rbt) {
 	return function() {
 		console.log(rbt);
 		document.getElementById(rbt+"_sun_button").setAttribute("class", "hidden");
+
 		var DOM_img = document.createElement("img");
 		DOM_img.src = "/images/Sunshine.png";
 		document.getElementById(rbt+"_sun").appendChild(DOM_img);
@@ -9,15 +10,12 @@ function give_sun(rbt) {
 
 }
 
-
-
-
 sun_buttons = document.getElementsByName("sunshine");
 rbts = document.getElementsByName("id_rbt");
   for (var i = 0; i < rbts.length; i++) {  
   	var rbt = rbts[i].innerHTML;
   	var button = document.getElementById(rbt+"_sun_button");
-  	if (button !== null) {
+  	if (button != null) {
   		document.getElementById(rbt+"_form").value = rbt;
     	button.addEventListener("click", give_sun(rbt), false);
     }
