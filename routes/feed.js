ec20a1ba969716c26688c244bcc745aa2ce70d23
yijuhou <this.yiju@gmail.com>
2014-06-03 02:29:40 -0700

    merged sunshine backend

--- routes/feed.js
+++ routes/feed.js
@@ -35,11 +35,11 @@ exports.post = function(req, res){
 	var pool = req.app.get('pool');
 	pool.getConnection(function(err, connection) {
 		connection.query('INSERT INTO sunshine(id_rbt_sun, id_giver) '+
-			'VALUES ('+req.body.id_rbt_sun+', '+ userFacebookId + ');', function(err, rows) {
+			'VALUES ('+req.body.id_rbt_sun+','+ userFacebookId + ');', function(err, rows) {
 			    connection.query('SELECT id_giver, photo_tag, id_rbt, rbt.id_user, '+
 	    	'DATE_FORMAT(created_at, "%Y-%m-%dT%TZ") as created_at, photo_rbt, rose, bud, thorn, '+
 			'IF(photo_tag="rose", 1, 0) as rosetag, IF(photo_tag="bud", 1, 0) as budtag,'+
-			'IF(photo_tag="thorn", 1, 0) as thorntag, IFNULL(id_giver, 0), (id_giver='+ userFacebookId +', 1, 0) as sun, '+
+			'IF(photo_tag="thorn", 1, 0) as thorntag, IFNULL(id_giver, 0), IF(id_giver='+ userFacebookId +', 1, 0) as sun, '+
 	    	'firstname, lastname, photo_user, facebook_id FROM rbt INNER JOIN user '+
 	    	'ON rbt.id_user = user.id_user '+
 	    	'LEFT JOIN sunshine ON id_rbt = id_rbt_sun '+
