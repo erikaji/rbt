c43e85ef8ee813063a5cda9a8807f79eae335fbf
yijuhou <this.yiju@gmail.com>
2014-06-03 02:58:29 -0700

    friend sunshine page has a bug

--- routes/friend.js
+++ routes/friend.js
@@ -7,15 +7,19 @@ exports.view = function(req, res){
 
 	var pool = req.app.get('pool');
 	var id_user = req.params.id;
+	var userFacebookId = req.user.id;
+
 	console.log(pool);
 	pool.getConnection(function(err, connection) {
-		connection.query('SELECT photo_tag, id_rbt, rbt.id_user, '+
-			'DATE_FORMAT(created_at, "%Y-%m-%dT%TZ") as created_at, photo_rbt, rose, bud, thorn,'+ 
+		connection.query('SELECT id_giver, photo_tag, id_rbt, rbt.id_user, '+
+	    	'DATE_FORMAT(created_at, "%Y-%m-%dT%TZ") as created_at, photo_rbt, rose, bud, thorn, '+
 			'IF(photo_tag="rose", 1, 0) as rosetag, IF(photo_tag="bud", 1, 0) as budtag,'+
-			'IF(photo_tag="thorn", 1, 0) as thorntag, '+
-			'user.id_user, firstname, lastname, photo_user, facebook_id FROM (rbt, user) '+
-			'WHERE (rbt.id_user = user.id_user AND rbt.id_user = ' + id_user +') '+
-			'ORDER BY created_at DESC;', function(err, rows_rbt) {
+			'IF(photo_tag="thorn", 1, 0) as thorntag, IFNULL(id_giver, 0), IF(id_giver='+ userFacebookId +', 1, 0) as sun, '+
+	    	'firstname, lastname, photo_user, facebook_id FROM rbt INNER JOIN user '+
+	    	'ON rbt.id_user = user.id_user '+
+	    	'LEFT JOIN sunshine ON id_rbt = id_rbt_sun '+
+	    	'WHERE rbt.id_user = '+ id_user +
+	    	' ORDER BY created_at DESC;', function(err, rows_rbt) {
 	    	res.render('friend', {
 				rbt: rows_rbt
 			});
@@ -23,3 +27,5 @@ exports.view = function(req, res){
 	    });
 	});
 };
+
+
