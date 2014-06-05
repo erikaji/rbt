8db26e0220a779c997286e72dc496853f98f4e94
erikaji <erikaji@stanford.edu>
2014-06-04 19:48:06 -0700

    friend fix 2

--- routes/friend.js
+++ routes/friend.js
@@ -10,7 +10,7 @@ exports.view = function(req, res){
 	pool.getConnection(function(err, connection) {
 		connection.query('SELECT id_rbt, rbt.id_user, firstname, lastname, photo_user, facebook_id,'+
 			'DATE_FORMAT(created_at, "%Y-%m-%dT%TZ") as created_at, photo_rbt, photo_tag, rose, bud, thorn, IF(photo_tag="rose", 1, 0) as rosetag, IF(photo_tag="bud", 1, 0) as budtag, IF(photo_tag="thorn", 1, 0) as thorntag, sun'
-			+' FROM rbt inner join (select id_rbt_sun, count(id_rbt_sun) as sun from sunshine group by id_rbt_sun) as sun_count on id_rbt_sun = id_rbt inner join user on rbt.id_user = user.id_user where rbt.id_user = '+ id_user +' ORDER BY created_at DESC;', function(err, rows_rbt) {
+			+' FROM rbt left join (select id_rbt_sun, count(id_rbt_sun) as sun from sunshine group by id_rbt_sun) as sun_count on id_rbt_sun = id_rbt inner join user on rbt.id_user = user.id_user where rbt.id_user = '+ id_user +' ORDER BY created_at DESC;', function(err, rows_rbt) {
 	    	res.render('friend', {
 				rbt: rows_rbt
 			});
